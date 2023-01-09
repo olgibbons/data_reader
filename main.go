d722a59aa00ff5b8d85ba98d75ae6b962d5075db
Al Norman <gibbonsdatascience@gmail.com>
2023-01-09 10:47:51 +0000

    Initial project version

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+func readCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	return records
+}
+
+func main() {
+	records := readCsvFile("username.csv")
+	fmt.Println(records)
+
+	fmt.Printf("There are %d records", len(records)-1)
+}
